0a41f260d0e660638832d0056c660cd42dd485cc
w11wo <wilsonwong961@gmail.com>
2022-12-15 19:03:32 +1100

    fixed commandline args

--- examples/pytorch/speech-recognition/run_distil_speech_recognition_ctc.py
+++ examples/pytorch/speech-recognition/run_distil_speech_recognition_ctc.py
@@ -63,10 +63,10 @@ def list_field(default=None, metadata=None):
 
 
 class DistillationTrainingArguments(TrainingArguments):
-    def __init__(self, *args, alpha=0.5, temperature=2.0, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.alpha = alpha
-        self.temperature = temperature
+    alpha: float = field(
+        default=0.5, metadata={"help": "Hyperparameter to control the relative strength of each loss."}
+    )
+    temperature: float = field(default=2.0, metadata={"help": "Scale factor of logits to soften the probabilities."})
 
 
 class DistillationTrainer(Trainer):
